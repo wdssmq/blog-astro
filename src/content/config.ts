d86994a8a5c54751107cd96e488ad75c852df9bd
wdssmq <wdssmq@qq.com>
2023-08-21 13:14:11 +0800

    style. 缩进统一

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,22 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z
+            .string()
+            .or(z.date())
+            .transform((val) => new Date(val)),
+        updatedDate: z
+            .string()
+            .optional()
+            .transform((str) => (str ? new Date(str) : undefined)),
+        heroImage: z.string().optional(),
         tags: z.array(z.string()),
-	}),
+    }),
 });
 
 export const collections = { blog };
