86e64da9b59849ab076c6cbd34717dca468a764e
wdssmq <wdssmq@qq.com>
2023-08-22 12:48:10 +0800

    up. tag 允许数字类型

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,7 +15,7 @@ const blog = defineCollection({
             .optional()
             .transform((str) => (str ? new Date(str) : undefined)),
         heroImage: z.string().optional(),
-        tags: z.array(z.string()),
+        tags: z.array(z.string().or(z.number())),
     }),
 });
 
